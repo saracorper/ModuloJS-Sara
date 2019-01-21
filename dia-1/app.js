4ed9f3ca4fa63ebe512fdc376e354c592daf0dc1
[saracorper] <[cordalpernassl@gmail.com]>
2019-01-21 21:32:42 +0100

    modificaciones día 1

---
+++ dia-1/app.js
@@ -0,0 +1,41 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+// console.log("Probando, probando, 1, 2, 3!");
+
+// console.log("I'm Javascript");
+// alert("I'm Javascript");
+
+// [1, 2].forEach(alert); //esto es un comentario
+
+// let nombre= "Sara";
+// let edad = 21;
+// const cumple;
+
+// alert("Mi nombre es " + nombre + " y tengo " + 21 + " años.");
+
+// console.log(`Mi nombre es ${nombre}`);
+
+// console.log("Mi nomnbre es", nombre);
+
+// const COLOR_ORANGE = "#FF7FF0";
+
+// let nombre =;
+
+let admin;
+let name = "John";
+admin = name;
+
+alert(admin);
+// Fin ejercicio 1.
+
+let ourPlanetName = "Tierra";
+let currentUserName;
+// Fin ejercicio 2.
+
+
+
+
+
