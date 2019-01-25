fd9aa8de0c37863935e6bd1ad1ec449cb732378b
[saracorper] <[cordalpernassl@gmail.com]>
2019-01-25 18:40:51 +0100

    crear appextra

---
+++ dia-5/app.js
@@ -0,0 +1,13 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+console.log("Probando, probando, 1, 2, 3!");
+
+
+
+
+
+
+
---
+++ dia-5/appextra.js
@@ -0,0 +1,7 @@
+
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+console.log("Probando, probando, 1, 2, 3!");
