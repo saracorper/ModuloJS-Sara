e919324f0025f963d2f68bdcc4c453c28363b130
[saracorper] <[cordalpernassl@gmail.com]>
2019-01-29 21:33:06 +0100

    Ejercicios dia 8

---
+++ dia-8/app.js
@@ -0,0 +1,88 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+
+console.log("Probando, probando, 1, 2, 3!");
+
+//EJERCICIO 1//
+
+/*function ucFirst(str) {
+    if (!str) return str;
+  
+    return str[0].toUpperCase() + str.slice(1);
+}
+  
+  alert( ucFirst("john") ); // John*/
+
+//EJERCICIO 2
+
+/*function checkSpam(str) {
+  let lowerStr = str.toLowerCase();
+
+  return lowerStr.includes("viagra") || lowerStr.includes("xxx");
+}
+
+alert(checkSpam("buy ViAgRA now"));
+alert(checkSpam("free xxxxx"));
+alert(checkSpam("innocent rabbit"));*/
+
+//EJERCICIO 3
+
+/*function truncate(str, maxlength) {
+  return str.length > maxlength ? str.slice(0, maxlength - 1) + "…" : str;
+}
+
+alert(truncate("SUPERCALIFRAGILISTICO", 6)); */
+
+//EJERCICIO 4
+
+/*function extraCurrencyValue(str) {
+  return +str.slice(1);
+}
+
+alert(extraCurrencyValue("$120")); */
+
+// EJERCICIO 5
+
+/*let string = prompt("Escriba algo", "");
+
+function checkPalindromo(str){
+    let minuscula = str.toLowerCase();
+
+    for (i = 0; i = str.length -1; i++) {
+        if (str[i] = str[str.length-1-i]
+
+} */
+
+// EJERCICIO 6
+
+/* let styles = ["Jazz", "Blues"];
+alert(styles);
+styles.push("Rock - n - Roll");
+alert(styles);
+styles[Math.trunc(styles.length / 2)] = "Classics";
+alert(styles);
+styles.shift();
+alert(styles);
+styles.unshift("Rap", "Reggae");
+alert(styles); */
+
+// EJERCICIO 7
+
+let arr = [];
+
+function sumImput() {
+  let valores = prompt("Introduce un valor");
+  arr.push(valores);
+
+}
+
+do {
+    sumImput();
+}while{
+    valores = number,
+}
+
+
+sumImput()
