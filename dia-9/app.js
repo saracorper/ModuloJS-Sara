cab54305ad12c91767ac530217c8f86dcf1cde32
[saracorper] <[cordalpernassl@gmail.com]>
2019-01-31 18:38:17 +0100

    Ejercicio dia-9

--- dia-9/app.js
+++ dia-9/app.js
@@ -4,3 +4,25 @@
  */
 
 console.log("Probando, probando, 1, 2, 3!");
+
+// EJERCICIO 1
+
+function maxSubSum(arr) {
+  let msf = 0;
+  let meh = 0;
+  let suma = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    meh += arr[i];
+    if (meh < 0) {
+      meh = 0;
+    }
+    if (msf < meh) {
+      msf = meh;
+      
+    }
+    return msf;
+  }
+}
+
+console.log(maxSubSum([-1, 2, 3, -9]));
