3faecba479136b504b3a3dd34a590f816c74c6c1
[saracorper] <[cordalpernassl@gmail.com]>
2019-02-02 18:31:40 +0100

    Ejercicios sabado tarde

--- dia-11/app.js
+++ dia-11/app.js
@@ -51,7 +51,7 @@ keys.push("more");
 alert(keys); */
 
 //EJERCICIO 5
-
+/*
 let messages = [
   { text: "Hello", from: "John" },
   { text: "How goes?", from: "John" },
@@ -59,5 +59,85 @@ let messages = [
 ];
 
 let readMap = new WeakMap();
+*/
 
-readMap.set(messages[0], new Date(2017, 1, 1));
+// EJERCICIO 6
+/*
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250
+};
+
+alert(sumSalaries(salaries)); // 650
+
+function sumSalaries(salaries) {
+  let suma = 0;
+  for (let value of Object.values(salaries)) {
+    suma += value;
+  }
+  return suma;
+}
+
+alert(sumSalaries(salaries));*/
+
+// EJERCICIO 7
+/*
+let user = {
+  name: "John",
+  age: 30
+};
+
+alert(count(user)); // 2
+
+function count(obj) {
+  return Object.keys(obj).length;
+}
+ */
+
+// EJERCICIO 8
+/*
+let user = {
+  name: "John",
+  years: 30
+};
+
+let { name, years: age, isAdmin = false } = user;
+
+console.log(isAdmin); */
+
+// EJERCICIO 9
+/*
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250
+};
+function topSalary(obj) {
+  let mayorSalario = 0;
+  let nombreMayorSalario = "";
+  for (let [name, salary] of Object.entries(obj)) {
+    if (salary > mayorSalario) {
+      mayorSalario = salary;
+      nombreMayorSalario = name;
+    }
+  }
+  return nombreMayorSalario || null;
+}
+
+alert(topSalary(salaries));*/
+
+//EJERCICIO 10
+/*
+let d = new Date(2012, 1, 20, 3, 12);
+alert(d); */
+
+// EJERCICIO 11
+/*
+let date = new Date(2012, 0, 3); 
+alert(getWeekDay(date)); 
+
+function getWeekDay(date) {
+  let corto = ["SU","Mo","TU","WE","TH","FR","SA"];
+  return corto[date.getDay()]; 
+} */
